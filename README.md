fdca1e728254d3940342ff2ff5c961d0cfed9ff4
Imran Khan <2023ac05619@wilp.bits-pilani.ac.in>
2025-08-02 22:17:16 +0300

    dockerized to run the predictions..

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # mlops-iris
+
+--- Deployment Complete! ---
+Grafana Dashboard: http://localhost:3000 (admin/admin)
+Prometheus UI:     http://localhost:9090
+API Health Check:  http://localhost:5001/health
+API Docs:          http://localhost:5001/
--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,18 @@ echo "--- Starting Local Deployment ---"
 set -e
 
 echo "[1/2] Stopping and removing any old containers..."
-docker-compose down
-echo "✅ Old containers removed."
+docker compose down
+echo "Old containers removed!"
 
 echo "[2/2] Starting the application, Prometheus, and Grafana..."
-docker-compose up --build -d
-echo "✅ Deployment stack is starting in the background."
+docker compose up --build -d
+echo "Deployment stack is starting in the background.."
 
 echo ""
-echo "--- 🚀 Deployment Complete! ---"
+echo "--- Deployment Complete! ---"
 echo "Grafana Dashboard: http://localhost:3000 (admin/admin)"
 echo "Prometheus UI:     http://localhost:9090"
 echo "API Health Check:  http://localhost:5001/health"
 echo "API Docs:          http://localhost:5001/"
 echo ""
-echo "Useful command: 'docker-compose logs -f' to view logs."
+echo "Useful command: 'docker compose logs -f' to view logs."
